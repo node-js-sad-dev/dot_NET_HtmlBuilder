a0a658c605bd7b00b8b4f2d4d8ee881283da464f
V.Piletskiy <v.piletskyi@lampalampa.net>
2022-12-29 22:08:23 +0200

    added meta header element

---
+++ HtmlBuilder/src/Elements/HeaderElements/WithoutNestedElements/Meta.cs
@@ -0,0 +1,13 @@
+﻿using HtmlBuilder.Properties;
+
+namespace HtmlBuilder.Elements.HeaderElements.WithoutNestedElements;
+
+public class Meta : HeaderElement
+{
+    public Meta() : base("meta", false, false, new List<PropertyDescription>()
+    {
+        new("charset", true),
+        new("name", true),
+        new("content", true)
+    }) {}
+}
